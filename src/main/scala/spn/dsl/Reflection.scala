d233d24e1d2f2c16ed26ea442ba5b4e4d3eb2afb
Mattia Matteini <matteinimattia@gmail.com>
2024-10-10 13:12:33 +0200

    refactor: update reflection signature

--- src/main/scala/spn/dsl/Reflection.scala
+++ src/main/scala/spn/dsl/Reflection.scala
@@ -10,16 +10,17 @@ object Reflection:
     * @return the [[SPN]]
     * @throws IllegalArgumentException if the input is malformed
     */
-  def reflect(input: String): SPN[String] =
+  def reflect(input: String): SPN[Any] =
     if input.isBlank || input.isEmpty then throw new IllegalArgumentException("Empty input")
     else
       val imports =
-        """import u07.modelling.SPN.SPN
-          |import u07.dsl.DSL.{*, given}
+        """import spn.SPN.*
+          |import spn.dsl.DSL.{*, given}
           |""".stripMargin
       try
         val res = dotty.tools.repl.ScriptEngine().eval(imports + input)
         res match
-          case spn: SPN[String] => spn
-          case trn: Trn[String] => Set(trn)
+          case spn: SPN[_] => spn.asInstanceOf[SPN[Any]]
+          case trn: Trn[_] => Set(trn.asInstanceOf[Trn[Any]])
+          case _ => throw new IllegalArgumentException("Invalid SPN")
       catch case e: Exception => throw new IllegalArgumentException(e.getMessage)
