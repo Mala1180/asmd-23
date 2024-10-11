d54c1d114fa20206a549b84d104aee99675517d8
Mattia Matteini <matteinimattia@gmail.com>
2024-10-11 11:24:53 +0200

    refactor: update OllamaClient

--- src/main/scala/spn/llm/OllamaClient.scala
+++ src/main/scala/spn/llm/OllamaClient.scala
@@ -1,29 +1,26 @@
 package spn.llm
 
 import requests.Response
-import spn.dsl.Reflection.reflect
-
 
 object OllamaClient extends App:
 
-  private enum Model(val name: String):
+  enum Model(val name: String):
     case Llama3 extends Model("spn-llama3-model")
     case Gemma2 extends Model("spn-gemma2-model")
-  
-  private val ollamaUrl = "http://localhost:11434/api/generate"
 
-  private val model = Model.Gemma2.name
-  private val prompt = "Provide me a Stochastic Petri Net representing a Stochastic Mutual Exclusion system with various rates"
-  private val data: String = s"""{ "model": "$model", "prompt": "$prompt", "stream": false }"""
-  val response: Response = requests.post(
-    ollamaUrl,
-    data = data,
-    readTimeout = 60000,
-    connectTimeout = 60000
-  )
+  private val prompt =
+    "Provide me a Stochastic Petri Net representing a Stochastic Mutual Exclusion system with various rates"
 
-  if response.statusCode == 200 then
-    val test = ujson.read(response.data.toString)
-    println(test("response").str)
-    println(reflect(test("response").str))
-  else println(s"Error: ${response.statusCode} ${response.statusMessage}")
+  def askToModel(model: Model, prompt: String): String =
+    val ollamaUrl = "http://localhost:11434/api/generate"
+    val data: String = s"""{ "model": "${model.name}", "prompt": "$prompt", "stream": false }"""
+    val response: Response = requests.post(
+      ollamaUrl,
+      data = data,
+      readTimeout = 60000,
+      connectTimeout = 60000
+    )
+    if response.statusCode == 200 then
+      val test = ujson.read(response.data.toString)
+      test("response").str
+    else throw new Exception(s"Error: ${response.statusCode} ${response.statusMessage}")
